e2dda461e121d4610d52a6f887028f54088071e4
Felix Klauke <info@felix-klauke.de>
2018-03-16 13:13:21 +0100

    Add complete block storage.
    Signed-off-by: Felix Klauke <info@felix-klauke.de>

--- src/DataBlock.cpp
+++ src/DataBlock.cpp
@@ -4,37 +4,37 @@
 
 #include "DataBlock.h"
 
-DataBlock::DataBlock(unsigned int *blockId, int *nextBlockId, unsigned int *dataSectionSize, char *dataSection)
+DataBlock::DataBlock(unsigned int blockId, int nextBlockId, unsigned int dataSectionSize, char dataSection)
         : blockId(blockId), nextBlockId(nextBlockId), dataSectionSize(dataSectionSize), dataSection(dataSection) {}
 
-unsigned int *DataBlock::GetBlockId() const {
+unsigned int DataBlock::GetBlockId() const {
     return blockId;
 }
 
-void DataBlock::SetBlockId(unsigned int *blockId) {
+void DataBlock::SetBlockId(unsigned int blockId) {
     DataBlock::blockId = blockId;
 }
 
-int *DataBlock::GetNextBlockId() const {
-    return nextBlockId;
+int *DataBlock::GetNextBlockId() {
+    return &nextBlockId;
 }
 
-void DataBlock::SetNextBlockId(int *nextBlockId) {
+void DataBlock::SetNextBlockId(int nextBlockId) {
     DataBlock::nextBlockId = nextBlockId;
 }
 
-unsigned int *DataBlock::GetDataSectionSize() const {
-    return dataSectionSize;
+unsigned int *DataBlock::GetDataSectionSize() {
+    return &dataSectionSize;
 }
 
-void DataBlock::SetDataSectionSize(unsigned int *dataSectionSize) {
+void DataBlock::SetDataSectionSize(unsigned int dataSectionSize) {
     DataBlock::dataSectionSize = dataSectionSize;
 }
 
-char *DataBlock::GetDataSection() const {
-    return dataSection;
+char *DataBlock::GetDataSection() {
+    return &dataSection;
 }
 
-void DataBlock::SetDataSection(char *dataSection) {
+void DataBlock::SetDataSection(char dataSection) {
     DataBlock::dataSection = dataSection;
 }
--- src/DataManager.cpp
+++ src/DataManager.cpp
@@ -23,19 +23,20 @@ DataBlock DataManager::ReadDataBlock(uint position) {
     char dataSection[dataSectionSize];
     dataStorage->ReadBlock(blockPosition + 8, dataSectionSize, dataSection);
 
-    return DataBlock(&position, &nextBlockId, &dataSectionSize, dataSection);
+    return DataBlock(position, nextBlockId, dataSectionSize, *dataSection);
 }
 
 void DataManager::WriteDataBlock(DataBlock dataBlock) {
-    uint initialPosition = blockSize * *dataBlock.GetBlockId();
+    uint initialPosition = blockSize * dataBlock.GetBlockId();
 
     uint headerSize = sizeof(dataBlock.GetNextBlockId()) + sizeof(dataBlock.GetDataSectionSize());
     char buffer[headerSize];
     memcpy(&buffer, dataBlock.GetNextBlockId(), sizeof(dataBlock.GetNextBlockId()));
-    memcpy(&buffer[sizeof(dataBlock.GetNextBlockId()) + 1], dataBlock.GetDataSectionSize(),
+    memcpy(&buffer[sizeof(dataBlock.GetNextBlockId())], dataBlock.GetDataSectionSize(),
            sizeof(dataBlock.GetDataSectionSize()));
 
     dataStorage->WriteBlock(initialPosition, headerSize, buffer);
-    dataStorage->WriteBlock(initialPosition + *dataBlock.GetDataSectionSize(), *dataBlock.GetDataSectionSize(),
-                            dataBlock.GetDataSection())
+    dataStorage->WriteBlockAndFlushOutput(initialPosition + *dataBlock.GetDataSectionSize(),
+                                          *dataBlock.GetDataSectionSize(),
+                                          dataBlock.GetDataSection());
 }
