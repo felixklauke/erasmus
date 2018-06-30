45bc3e914218df067655b50153206e452691d38d
Felix Klauke <info@felix-klauke.de>
2018-06-30 11:17:49 +0200

    Write basic fixture with test setup for file system accessor test.
    Signed-off-by: Felix Klauke <info@felix-klauke.de>

--- test/c++/FileSystemAccessorTest.cpp
+++ test/c++/FileSystemAccessorTest.cpp
@@ -5,7 +5,39 @@
 #include <include/FileSystemAccessor.h>
 #include "gtest/gtest.h"
 
-TEST(FileSystemAccesor, testRead) {
-    FileSystemAccessor *fileSystemAccessor = new FileSystemAccessor("");
-    EXPECT_EQ(1, 1);
+
+class FileSystemAccessorTest : public ::testing::Test {
+
+protected:
+    FileSystemAccessor *fileSystemAccessor;
+
+    void SetUp() override {
+        std::ofstream o("test.acc");
+        fileSystemAccessor = new FileSystemAccessor("test.acc");
+    }
+
+
+    void TearDown() override {
+        remove("test.acc");
+        delete (fileSystemAccessor);
+    }
+};
+
+TEST_F(FileSystemAccessorTest, testRead) {
+    auto *array = new char[3];
+    array[0] = 5;
+    array[1] = 8;
+    array[2] = 12;
+    fileSystemAccessor->Write(0, 3, array);
+
+    auto *result = new char[3];
+    fileSystemAccessor->Read(0, 3, result);
+
+    EXPECT_EQ(array[0], result[0]);
+    EXPECT_EQ(array[1], result[1]);
+    EXPECT_EQ(array[2], result[2]);
+
+
+    delete (array);
+    delete (result);
 }
